b0c6295c9dbfc84642f3a94f372a0acfda038e7e
felipemv197-ux <felipemv197@gmail.com>
2026-09-19 16:35:19 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,20 @@
-const CACHE_NAME = 'panaderia-v1';
-const urlsToCache = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'panaderia-v2';
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+  // Si la petición es para productos.json, obtener siempre la versión más reciente de internet
+  if (event.request.url.includes('productos.json')) {
+    event.respondWith(fetch(event.request));
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(response => response || fetch(event.request))
+    );
+  }
 });
-
